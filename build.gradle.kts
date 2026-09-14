314f6c713f2adbe0c5b4740b796dbb4eea871b5a
ekodoank89 <ayaekodoank@gmail.com>
2026-09-14 18:39:06 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") apply false
-    id("org.jetbrains.kotlin.plugin.compose") apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
+

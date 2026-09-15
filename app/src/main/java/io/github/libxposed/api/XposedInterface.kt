c521706091eb202bb7184eab003517e19775349f
ekodoank89 <ayaekodoank@gmail.com>
2026-09-16 05:36:10 +0700

    Create XposedInterface.kt

---
+++ app/src/main/java/io/github/libxposed/api/XposedInterface.kt
@@ -0,0 +1,9 @@
+package io.github.libxposed.api
+
+/**
+ * STUB compile-only.
+ * Implementasi asli disediakan framework LSPosed saat runtime
+ * (di-inject ke classloader), sehingga stub ini hanya dipakai saat kompilasi
+ * dan otomatis tertimpa (shadowed) oleh versi asli di runtime.
+ */
+interface XposedInterface

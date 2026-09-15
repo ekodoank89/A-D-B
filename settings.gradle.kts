084fafd2f34f7146e03702fa04b05a157f1fadcc
ekodoank89 <ayaekodoank@gmail.com>
2026-09-15 15:43:36 +0700

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+                includeGroupAndSubgroups("androidx")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOSITORIES)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "A-Y-A"
+include(":app")

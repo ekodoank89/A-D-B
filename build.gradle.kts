c25c2850147c0e6581d71d8d2fe09dd424f97d7a
ekodoank89 <ayaekodoank@gmail.com>
2026-09-15 17:15:44 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,12 @@
-buildscript {
+pluginManagement {
     repositories {
         google()
         mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.0")
+        gradlePluginPortal()
     }
 }
 
-
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.7.0" apply false
+    // plugin lainnya...
 }

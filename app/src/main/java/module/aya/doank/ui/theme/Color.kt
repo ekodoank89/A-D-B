877b7b0f937fbeee7124ad4a283f6f077cc3c176
ekodoank89 <ayaekodoank@gmail.com>
2026-09-15 15:24:14 +0700

    Create Color.kt

---
+++ app/src/main/java/module/aya/doank/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package module.aya.doank.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xD0BCFF)
+val PurpleGrey80 = Color(0xCCC2DC)
+val Pink80 = Color(0xEFB8C8)
+
+val Purple40 = Color(0x6650a4)
+val PurpleGrey40 = Color(0x625b71)
+val Pink40 = Color(0x7D5260)

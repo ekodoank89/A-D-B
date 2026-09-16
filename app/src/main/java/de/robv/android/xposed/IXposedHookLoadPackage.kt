cfd31e8fc4eca0af4850d77a32b110b4648493bc
ekodoank89 <ayaekodoank@gmail.com>
2026-09-17 00:35:51 +0700

    Delete app/src/main/java/de/robv/android/xposed directory

--- app/src/main/java/de/robv/android/xposed/IXposedHookLoadPackage.kt
+++
@@ -1,8 +0,0 @@
-package de.robv.android.xposed
-
-import de.robv.android.xposed.callbacks.XC_LoadPackage
-
-/** STUB — entry point modul legacy. */
-interface IXposedHookLoadPackage {
-    fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam)
-}

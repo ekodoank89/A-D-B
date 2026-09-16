2054cabc5ad36b7f4514cba097d1ccaa98e5d478
ekodoank89 <ayaekodoank@gmail.com>
2026-09-16 07:06:20 +0700

    Delete app/src/main/java/de/robv/android/xposed/IXposedHookLoadPackage.kt

--- app/src/main/java/de/robv/android/xposed/IXposedHookLoadPackage.kt
+++
@@ -1,8 +0,0 @@
-package de.robv.android.xposed
-
-import de.robv.android.xposed.callbacks.XC_LoadPackage
-
-/** STUB compile-only — versi asli disediakan framework LSPosed saat runtime. */
-interface IXposedHookLoadPackage {
-    fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam)
-}

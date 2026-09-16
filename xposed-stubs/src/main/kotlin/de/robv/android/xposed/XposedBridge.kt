ad36787fe00fdade962df1b3762107a157ac0de1
ekodoank89 <ayaekodoank@gmail.com>
2026-09-16 07:32:23 +0700

    Delete xposed-stubs directory

--- xposed-stubs/src/main/kotlin/de/robv/android/xposed/XposedBridge.kt
+++
@@ -1,8 +0,0 @@
-package de.robv.android.xposed
-
-/** STUB compile-only — versi asli disediakan framework LSPosed saat runtime. */
-object XposedBridge {
-    fun log(text: String) {
-        android.util.Log.i("XposedBridge", text)
-    }
-}

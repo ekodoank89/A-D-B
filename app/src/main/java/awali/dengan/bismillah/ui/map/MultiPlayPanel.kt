1f49111d22c3ccc22b886f77c704f3b63378021e
ekodoank89 <ayaekodoank@gmail.com>
2026-09-17 12:53:30 +0700

    Update MultiPlayPanel.kt

--- app/src/main/java/awali/dengan/bismillah/ui/map/MultiPlayPanel.kt
+++ app/src/main/java/awali/dengan/bismillah/ui/map/MultiPlayPanel.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight

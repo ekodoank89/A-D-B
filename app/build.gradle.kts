c8832e47d9bdd777527f794adfe10289d412ff27
ekodoank89 <ayaekodoank@gmail.com>
2026-09-17 19:05:51 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "awali.dengan.bismillah"
         minSdk = 30    // Android 11
         targetSdk = 35 // Android 15
-        versionCode = 39
-        versionName = "2.9.0"
+        versionCode = 41
+        versionName = "2.9.2"
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
     }
 
